fa5edfdd96ee28f21b9470c4096991792f4fcd62
Giovanni Gonzaga <giovannifgonzaga@gmail.com>
2023-07-09 15:30:23 -0500

    create search page and layout

---
+++ app/search/layout.tsx
@@ -0,0 +1,13 @@
+export default function SearchLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <div className="mx-auto flex max-w-7xl flex-col bg-white py-6 text-black dark:bg-black dark:text-white md:flex-row">
+            <div className="order-first flex-none md:w-1/6">
+                <h1>Collections</h1>
+            </div>
+            <div className="order-last min-h-screen w-full md:order-none">{children}</div>
+            <div className="order-none md:order-last md:w-1/6 md:flex-none">
+                <h1>Filtering</h1>
+            </div>
+        </div>
+    )
+}
---
+++ app/search/page.tsx
@@ -0,0 +1,5 @@
+export default async function SearchPage() {
+    return (
+        <h1>Search Page Here</h1>
+    )
+}
