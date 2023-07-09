cad07aa718e48e1f3f01ec6354e6e2ab33613dde
Giovanni Gonzaga <giovannifgonzaga@gmail.com>
2023-07-09 17:51:57 -0500

    rounded borders search

--- components/layout/navbar/search.tsx
+++ components/layout/navbar/search.tsx
@@ -2,13 +2,13 @@ import SearchIcon from "../../icons/search";
 
 export default function Search(){
     return (
-        <form className="relative m-0 flex lg:w-full items-center border border-gray-200 bg-transparent p-0 dark:border-gray-500">
+        <form className="relative m-0 flex lg:w-full items-center border border-gray-200 bg-transparent p-0 dark:border-gray-500 rounded-full">
             <input
                 type="text"
                 name="search"
                 placeholder="Search for blends..."
                 autoComplete="off"
-                className="w-full px-4 py-2 text-black bg-black bg-opacity-50 text-gray-100"
+                className="w-full px-4 py-2 text-black bg-black bg-opacity-50 text-gray-100 rounded-full"
             />
             <div className="absolute right-0 top-0 mr-3 flex h-full items-center">
                 <SearchIcon className="h-5" />
