a61ad15e70b979c0c165eebbced5126ba42874c7
Giovanni Gonzaga <giovannifgonzaga@gmail.com>
2023-07-09 11:42:23 -0500

    add hover animation to logo

--- components/layout/navbar/index.tsx
+++ components/layout/navbar/index.tsx
@@ -7,7 +7,7 @@ export default function Navbar() {
     return (
         <nav className="relative flex items-center justify-between bg-white p-4 dark:bg-black lg:px-6"> 
             <div className="flex justify-self-center md:w-1/3 md:justify-self-start">
-                <div className="md:mr-4 flex-shrink-0 cursor-pointer">
+                <div className="md:mr-4 flex-shrink-0 cursor-pointer transition-all ease-in-out hover:scale-110">
                     <Link href="/" aria-label="Go back home">
                         <Image src="/interstellar-logo.jpg" alt="Logo" width={30} height={30} layout='fixed' />
                     </Link>
