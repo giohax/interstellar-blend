dafdbdf1b5425624b33f74b9e0225e22cdcf85a1
Giovanni Gonzaga <giovannifgonzaga@gmail.com>
2023-07-09 11:22:36 -0500

    add search icon to search bar

--- components/layout/navbar/search.tsx
+++ components/layout/navbar/search.tsx
@@ -1,3 +1,5 @@
+import SearchIcon from "./icons/search";
+
 export default function Search(){
     return (
         <form className="relative m-0 flex w-full items-center border border-gray-200 bg-transparent p-0 dark:border-gray-500">
@@ -8,6 +10,9 @@ export default function Search(){
                 autoComplete="off"
                 className="w-full px-4 py-2 text-black dark:bg-black dark:text-gray-100"
             />
+            <div className="absolute right-0 top-0 mr-3 flex h-full items-center">
+                <SearchIcon className="h-5" />
+            </div>
         </form>
     )
 }
