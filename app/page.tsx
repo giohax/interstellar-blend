dfcda38cf3d2134ca087e4c2083c411d42c19d6d
Giovanni Gonzaga <giovannifgonzaga@gmail.com>
2023-07-10 17:28:52 -0500

    small fix

--- app/page.tsx
+++ app/page.tsx
@@ -15,13 +15,15 @@ export default function Home() {
                 muted 
                 loop 
                 onPause={()=>setShowFallbackImage(true)}
+                onError={()=>setShowFallbackImage(true)}
+                onSuspend={()=>setShowFallbackImage(true)}
                 >
                 <source src="/videos/energy-radiating-720p.mp4" type="video/mp4" />
               </video>
             ) : (
               <div className="absolute inset-0 min-w-full min-h-full bg-center bg-cover">
                 <Image
-                  src="/images/interstellar-blend-bg.png"
+                  src="/images/energy-radiating-pic.png"
                   alt="Video Fallback"
                   layout="fill"
                   objectFit="cover"
@@ -32,13 +34,6 @@ export default function Home() {
         <div className="absolute inset-0 flex items-center justify-center flex-col">
           <h1 className="text-white text-3xl lg:text-6xl font-bold font-vandiana text-center tracking-tighter">INTERSTELLAR BLEND</h1>
           <h2 className="text-white text-lxl lg:text-2xl font-bold font-vandiana text-center ">BECOME LIMITLESS WITH OUR PROPRIETARY HERBS</h2>
-          <video className="inset-0 w-1/3 h-1/3 object-cover"
-                autoPlay 
-                muted 
-                loop
-                >
-                <source src="/videos/energy-radiating-720p.mp4" type="video/mp4" />
-              </video>
           <Link href="/search" className="mt-8 bg-white hover:bg-gray-100 text-black text-md lg:text-lg font-normal py-3 px-6 rounded-full shadow-lg font-vandiana cursor-pointer">SHOP NOW</Link>
         </div>
 
