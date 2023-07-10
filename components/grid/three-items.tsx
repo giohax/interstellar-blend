c0ff2b9e0e529fcd8e8be879a148dec6aeff2d31
Giovanni Gonzaga <giovannifgonzaga@gmail.com>
2023-07-10 08:54:49 -0500

    create grid and mock products

---
+++ components/grid/three-items.tsx
@@ -0,0 +1,71 @@
+import { GridTileImage } from 'components/grid/tile';
+import Link from 'next/link';
+import mockProducts from 'mock-db/mock-db.json';
+
+type Product = {
+  handle: string;
+  featuredImage: { url: string };
+  title: string;
+  priceRange: {
+    maxVariantPrice: {
+      amount: string;
+      currencyCode: string;
+    };
+  };
+};
+
+
+function ThreeItemGridItem({
+  item,
+  size,
+  background
+}: {
+  item: Product;
+  size: 'full' | 'half';
+  background: 'white' | 'pink' | 'purple' | 'black';
+}) {
+  return (
+    <div
+      className={size === 'full' ? 'lg:col-span-4 lg:row-span-2' : 'lg:col-span-2 lg:row-span-1'}
+    >
+      <Link className="block h-full" href={`/product/${item.handle}`}>
+        <GridTileImage
+          src={item.featuredImage.url}
+          width={size === 'full' ? 1080 : 540}
+          height={size === 'full' ? 1080 : 540}
+          priority={true}
+          background={background}
+          alt={item.title}
+          labels={{
+            title: item.title as string,
+            amount: item.priceRange.maxVariantPrice.amount,
+            currencyCode: item.priceRange.maxVariantPrice.currencyCode
+          }}
+        />
+      </Link>
+    </div>
+  );
+}
+
+function getCollectionProducts({collection}: {collection:string}): Promise<Product[]>{
+  return Promise.resolve(mockProducts);
+}
+
+export async function ThreeItemGrid() {
+  // Collections that start with `hidden-*` are hidden from the search page.
+  const homepageItems = await getCollectionProducts({
+    collection: 'hidden-homepage-featured-items'
+  });
+
+  if (!homepageItems[0] || !homepageItems[1] || !homepageItems[2]) return null;
+
+  const [firstProduct, secondProduct, thirdProduct] = homepageItems;
+
+  return (
+    <section className="lg:grid lg:grid-cols-6 lg:grid-rows-2" data-testid="homepage-products">
+      <ThreeItemGridItem size="full" item={firstProduct} background="purple" />
+      <ThreeItemGridItem size="half" item={secondProduct} background="black" />
+      <ThreeItemGridItem size="half" item={thirdProduct} background="pink" />
+    </section>
+  );
+}
