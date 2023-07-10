0e85aeb9634255dec80a4898cf7a21dc54a3e190
Giovanni Gonzaga <giovannifgonzaga@gmail.com>
2023-07-10 15:47:35 -0500

    add controls attribute to video

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <>
       <div className="fixed inset-0 flex items-center justify-center overflow-hidden">
-        <video className="absolute inset-0 min-w-full min-h-full object-cover" autoPlay muted loop playsInline>
+        <video controls={false} className="absolute inset-0 min-w-full min-h-full object-cover" autoPlay muted loop playsInline>
           <source src="/videos/energy-radiating-720p.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
