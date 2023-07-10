912977dad20f9a488f1b0823ea915356034c3fa7
Giovanni Gonzaga <giovannifgonzaga@gmail.com>
2023-07-10 15:50:52 -0500

    slight changes

--- app/page.tsx
+++ app/page.tsx
@@ -6,9 +6,8 @@ export default function Home() {
   return (
     <>
       <div className="fixed inset-0 flex items-center justify-center overflow-hidden">
-        <video controls={false} className="absolute inset-0 min-w-full min-h-full object-cover" autoPlay muted loop playsInline>
+        <video controls={false} className="absolute inset-0 min-w-full min-h-full object-cover" autoPlay muted loop>
           <source src="/videos/energy-radiating-720p.mp4" type="video/mp4" />
-          Your browser does not support the video tag.
         </video>
         <div className="absolute inset-0 bg-black opacity-10"></div>
         <div className="absolute inset-0 flex items-center justify-center flex-col">
