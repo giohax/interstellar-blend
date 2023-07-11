f25efb3059dfdf7db9545c99e1c9baa5b0f4514a
Giovanni Gonzaga <giovannifgonzaga@gmail.com>
2023-07-10 23:28:34 -0500

    fix text size

--- app/page.tsx
+++ app/page.tsx
@@ -40,13 +40,13 @@ export default function Home() {
         <div className="absolute inset-0 bg-black opacity-10"></div>
         <div className="absolute inset-0 flex items-center justify-center flex-col px-4">
           <h1 className="text-white text-5xl lg:text-6xl font-bold font-vandiana text-center tracking-tighter custom-shadow leading-none">INTERSTELLAR BLEND</h1>
-          <h2 className="text-white text-2xl lg:text-3xl font-bold font-vandiana text-center custom-shadow">BECOME LIMITLESS WITH OUR PROPRIETARY HERBS</h2>
+          <h2 className="text-white text-1xl lg:text-2xl font-bold font-vandiana text-center custom-shadow">BECOME LIMITLESS WITH OUR PROPRIETARY HERBS</h2>
           <Link href="/search" className="mt-8 bg-white hover:bg-red-500 hover:text-white text-black text-md lg:text-lg font-bold py-3 px-6 w-full md:w-64 rounded-full shadow-lg text-center font-vandiana cursor-pointer">SHOP NOW</Link>
           <Link href="/search" className="mt-2 bg-custom-black hover:bg-red-500 text-white text-md lg:text-lg font-bold py-3 px-6 w-full md:w-64 rounded-full shadow-lg text-center font-vandiana cursor-pointer">LEARN MORE</Link>
         </div>
         <style jsx>{`
           .custom-shadow {
-            text-shadow: 0 0 10px rgba(255, 255, 255, 0.5);  // dark shadow for small screens
+            text-shadow: 0 0 10px rgba(0, 0, 0, 0.5);  // dark shadow for small screens
           }
 
           @media (min-width: 1024px) {
