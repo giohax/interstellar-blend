25aeb6a2afafaf6c6fb30005b4fe1ffcb88690fa
Giovanni Gonzaga <giovannifgonzaga@gmail.com>
2023-07-11 12:55:30 -0500

    latest

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 "use client"
+import BoltIcon from '@/components/icons/bolt';
 import Link from 'next/link';
 
 export default function Home() {
@@ -37,7 +38,7 @@ export default function Home() {
           <div className="absolute inset-0 bg-black opacity-10"></div>
           <div className="absolute inset-0 flex items-center justify-center flex-col px-4">
             <h1 className="text-red-500 md:text-white text-5xl lg:text-6xl font-bold font-vandiana text-center tracking-tighter custom-shadow leading-none">INTERSTELLAR BLEND</h1>
-            <h2 className="text-white text-1xl lg:text-2xl font-bold font-vandiana text-center custom-subtitle-shadow">BECOME LIMITLESS WITH OUR PROPRIETARY HERBS</h2>
+            <h2 className="text-white text-1xl lg:text-2xl font-bold font-vandiana text-center custom-subtitle-shadow">BECOME LIMITLESS WITH OUR ANTI-AGING HERBS</h2>
             <Link href="/search" className="mt-8 bg-white hover:bg-red-500 hover:text-white text-black text-md lg:text-lg font-bold py-3 px-6 w-full md:w-64 rounded-full shadow-lg text-center font-vandiana cursor-pointer">SHOP NOW</Link>
             <Link href="" className="mt-2 bg-custom-black hover:bg-red-500 text-white text-md lg:text-lg font-bold py-3 px-6 w-full md:w-64 rounded-full shadow-lg text-center font-vandiana cursor-pointer border-2 border-red-500">LEARN MORE</Link>
           </div>
@@ -117,8 +118,8 @@ export default function Home() {
             <h1 className="text-xl md:text-3xl font-bold text-purple-500 font-vandiana">Fast</h1>
             <h1 className="text-4xl md:text-5xl font-bold mt-6">Your way to longevity</h1>
             <h2 className="text-l md:text-xl mt-6 text-gray-400 w-5/6 max-w-740 mx-auto">
-              Our ingredients are hand-picked to naturally curb appetite and sustain energy levels. The blends complement your 
-              fasting journey, supports detoxification, and promotes longevity. With our potent, natural ingredients fueling your 
+              Our ingredients are hand-picked to naturally curb appetite and sustain energy levels. The blends complement your
+              fasting journey, supports detoxification, and promotes longevity. With our potent, natural ingredients fueling your
               body, step into a healthier, longer life.
             </h2>
           </div>
