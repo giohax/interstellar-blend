139c7482cefa35002be98f489ee3ed9db18ae69d
Giovanni Gonzaga <giovannifgonzaga@gmail.com>
2023-07-11 11:54:14 -0500

    testing

--- app/page.tsx
+++ app/page.tsx
@@ -72,8 +72,8 @@ export default function Home() {
                 <img className="rounded-md" src="images/ingredients.jpeg" />
               </div>
 
-              {/* <div className="flex flex-col justify-between leading-relaxed space-y-10">
-                <div className="text-center md:text-start">
+              <div className="flex flex-col justify-between leading-relaxed space-y-10">
+                {/* <div className="text-center md:text-start">
                   <h1 className="font-bold text-2xl mb-2">Feel young again</h1>
                   <p className="text-gray-400">
                     Rekindle the vitality of your youth with our organic herb blend. It's more than just a drink; it's a
@@ -98,8 +98,8 @@ export default function Home() {
                     natural ingredients, it's the fuel you need to break through boundaries and embrace the limitless
                     possibilities of your day.
                   </p>
-                </div>
-              </div> */}
+                </div> */}
+              </div>
             </div>
           </div>
         </div>
