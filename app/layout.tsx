e995fb1d88db069153424bb6b48687cbc1f15a62
Giovanni Gonzaga <giovannifgonzaga@gmail.com>
2023-07-10 20:27:07 -0500

    increase size of text

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import '../styles/fonts.css'
+import { Suspense } from 'react';
 
 
 const inter = Inter({
@@ -25,9 +26,9 @@ export default function RootLayout({
     <html lang="en" className={inter.variable}>
       <body className="bg-black text-white selection:bg-fuchsia-600 selection:text-white">
         <Navbar />
-        <main>
-          {children}
-        </main>
+        <Suspense>
+          <main>{children}</main>
+        </Suspense>
       </body>
     </html>
   )
