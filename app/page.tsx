5a55a24313b7d4c4883f3a9a7a3ba026a52f138e
Giovanni Gonzaga <giovannifgonzaga@gmail.com>
2023-07-12 12:10:55 -0500

    fix apostrophe

--- app/page.tsx
+++ app/page.tsx
@@ -191,7 +191,7 @@ export default function Home() {
             <h1 className="text-4xl md:text-5xl font-bold mt-6">Others on the same path as you</h1>
             <h2 className="text-l md:text-xl mt-6 text-gray-400 w-5/6 max-w-740 mx-auto">
               Join a community of people who have transformed their lives with our blends. Their stories echo triumph, resilience, and a 
-              renewed zest for life. With us, you're not just choosing a product, you're embracing a journey towards your best self. Hop 
+              renewed zest for life. With us, you&apos;re not just choosing a product, you&apos;re embracing a journey towards your best self. Hop 
               on this path of transformation, because together, we can make it happen.
             </h2>
           </div>
