b271ebf6bf4dadc5d76b3bf8a5103183262d1afb
Giovanni Gonzaga <giovannifgonzaga@gmail.com>
2023-07-12 10:13:12 -0500

    smaller text

--- app/page.tsx
+++ app/page.tsx
@@ -46,7 +46,7 @@ export default function Home() {
 
           <div className="absolute inset-0 bg-black opacity-10"></div>
           <div className="absolute inset-0 flex items-center justify-center flex-col px-10">
-            <h1 className="text-red-500 md:text-white text-5xl lg:text-6xl font-bold font-vandiana text-center tracking-tighter custom-shadow leading-none">INTERSTELLAR BLEND</h1>
+            <h1 className="text-red-500 md:text-white text-4xl lg:text-6xl font-bold font-vandiana text-center tracking-tighter custom-shadow leading-none">INTERSTELLAR BLEND</h1>
             <h2 className="text-white text-1xl lg:text-2xl font-bold font-vandiana text-center custom-subtitle-shadow">BECOME LIMITLESS WITH OUR ANTI-AGING HERBS</h2>
             <Link href="/search" className="mt-8 bg-white hover:bg-red-500 hover:text-white text-black text-md lg:text-lg font-bold py-3 px-6 w-full md:w-64 rounded-full shadow-lg text-center font-vandiana cursor-pointer">SHOP NOW</Link>
             <a href="#section" onClick={handleClick} className="mt-2 bg-custom-black hover:bg-red-500 text-white text-md lg:text-lg font-bold py-3 px-6 w-full md:w-64 rounded-full shadow-lg text-center font-vandiana cursor-pointer border-2 border-red-500">LEARN MORE</a>
