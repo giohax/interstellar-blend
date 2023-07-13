6832d5a6d078dde7444e8bfc8459235cf9f2f81f
Giovanni Gonzaga <giovannifgonzaga@gmail.com>
2023-07-13 16:11:25 -0500

    translucent

--- components/layout/navbar/search.tsx
+++ components/layout/navbar/search.tsx
@@ -9,6 +9,7 @@ export default function Search(){
                 placeholder="Search for blends..."
                 autoComplete="off"
                 className="w-full px-4 py-2 text-black bg-black bg-opacity-50 text-gray-100 rounded-full"
+                style={{ backdropFilter: 'blur(10px)' }}
             />
             <div className="absolute right-0 top-0 mr-3 flex h-full items-center">
                 <SearchIcon className="h-5" />
