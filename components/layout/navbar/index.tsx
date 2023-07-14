feacf64d2936579beb208caa0458d9f579e22641
Giovanni Gonzaga <giovannifgonzaga@gmail.com>
2023-07-14 14:08:27 -0500

    fix logo for mobile

--- components/layout/navbar/index.tsx
+++ components/layout/navbar/index.tsx
@@ -14,20 +14,23 @@ export default function Navbar() {
 
 
     const pathname = usePathname()
-    
+
     return (
-        <nav className="relative flex items-center justify-between p-4 lg:px-6 fixed z-10 max-w-8xl mx-auto lg:h-28"> 
-            <div className="">
+        <nav className="relative flex items-center justify-between p-4 lg:px-6 fixed z-10 max-w-8xl mx-auto lg:h-28">
+            <div className="w-1/3">
                 <MobileMenu />
             </div>
-            <div className="absolute left-0 right-0">
+            <div className="w-1/3">
                 {
-                    pathname !== '/' && <Image src="/interstellar-logo.jpg" height={25} width={25} alt="logo" className="mx-auto block" />
+                    pathname !== '/' && 
+                        <Link href={"/"}>
+                            <Image src="/interstellar-logo.jpg" height={25} width={25} alt="logo" className="mx-auto" />
+                        </Link>
                 }
             </div>
-            <div className="flex space-x-6">
+            <div className="flex space-x-6 w-1/3 justify-end">
                 <SearchIcon className="h-6" />
-                <CartIcon/>
+                <CartIcon />
             </div>
         </nav>
     )
