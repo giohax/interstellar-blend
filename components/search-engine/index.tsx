15f0a5790681d47f76455bff3ddb1149273be0db
Giovanni Gonzaga <giovannifgonzaga@gmail.com>
2023-07-15 17:23:34 -0500

    search bar blur

--- components/search-engine/index.tsx
+++ components/search-engine/index.tsx
@@ -5,14 +5,13 @@ export default function SearchEngine({ className }: { className?: string }) {
 
     return (
         <>
-            <form className="relative max-w-lg m-0 flex w-full items-center bg-transparent p-0 border-gray-500 border rounded-lg">
+            <form className="relative max-w-lg m-0 flex w-full items-center bg-transparent p-0 border-gray-500 border rounded-lg backdrop-blur-md">
                 <input
                     type="text"
                     name="search"
                     placeholder="Type in your symptoms..."
                     autoComplete="off"
                     className="w-full pl-10 pr-4 py-3 text-black bg-black bg-opacity-50 text-gray-100 rounded-lg"
-                    style={{ backdropFilter: 'blur(10px)' }}
                     onFocus={(event) => event.preventDefault()}
                 />
                 <div className="absolute left-0 top-0 ml-3 flex h-full items-center">
