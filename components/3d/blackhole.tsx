3c662303a0a2718b7c8ac79c699167af09ea7e4f
Giovanni Gonzaga <giovannifgonzaga@gmail.com>
2023-07-15 17:04:32 -0500

    add rotation to black hole

--- components/3d/blackhole.tsx
+++ components/3d/blackhole.tsx
@@ -1,6 +1,15 @@
 import { useGLTF } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
 
 export default function Blackhole() {
     const gltf = useGLTF("/models/blackhole/scene.gltf");
+
+    useFrame((state, delta) => {
+        if (gltf.scene) {
+            gltf.scene.rotation.y += delta;
+        }
+    });
+
+    
     return <primitive object={gltf.scene} rotation={[9.5, Math.PI / 2, 0]} />;
 }
